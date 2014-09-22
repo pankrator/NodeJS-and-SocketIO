e4f512aaf0362bff01ea6e491db155fe0f9b08c5
Nikolay Stoykov <nikolaisstoykov@gmail.com>
2014-09-22 10:05:02 +0300

    Collision detection algorithm re-writen in Typescript.

--- IsoGame-Client/src/script/Server/public/physics/algorithms.js
+++ IsoGame-Client/src/script/Server/public/physics/algorithms.js
@@ -1,105 +1,107 @@
-var broadphase = {};
+var MainModule;
+(function (MainModule) {
+    var SpatialGrid = (function () {
+        function SpatialGrid(minX, maxX, minY, maxY, cellSize, entities) {
+            this.cellSize = cellSize;
+            this.minX = minX;
+            this.maxX = maxX;
+            this.minY = minY;
+            this.maxY = maxY;
+            this.entities = entities;
 
-broadphase.spatialGrid = function (minX, maxX, minY, maxY, cellSize, entities) {
-    this.cellSize = cellSize;
-    this.minX = minX;
-    this.maxX = maxX;
-    this.minY = minY;
-    this.maxY = maxY;
-    this.entities = entities;
-
-    this.cGridWith = Math.floor((this.maxX - this.minX) / this.cellSize);
-    this.cGridHeight = Math.floor((this.maxY - this.minY) / this.cellSize);
-    this.totalCells = this.cGridWith * this.cGridHeight;
-
-    this.grid = [[]];
-};
-
-broadphase.spatialGrid.prototype.update = function () {
-    var entity, cXEntityMin, cXEntityMax, cYEntityMin, cYEntityMax, cX, cY, gridCol, gridCell;
-
-    this.grid = Array(this.cGridWidth);
-
-    for (i = 0; i < this.entities.length; i++) {
-        entity = this.entities[i];
-
-        //Check whether entity is outside the spatial grid
-        if (entity.x < this.minX || entity.x > this.maxX || entity.y < this.minY || entity.y > this.maxY) {
-            continue;
+            this.cGridWidth = Math.floor((this.maxX - this.minX) / this.cellSize);
+            this.cGridHeight = Math.floor((this.maxY - this.minY) / this.cellSize);
+            this.totalCells = this.cGridWidth * this.cGridHeight;
         }
+        SpatialGrid.prototype.update = function () {
+            var entity, cXEntityMin, cXEntityMax, cYEntityMin, cYEntityMax, cX, cY, gridCol, gridCell;
 
-        cXEntityMin = Math.floor((entity.x - this.minX) / this.cellSize);
-        cXEntityMax = Math.floor((entity.x + entity.sizeX - this.minX) / this.cellSize);
-        cYEntityMin = Math.floor((entity.y - this.minY) / this.cellSize);
-        cYEntityMax = Math.floor((entity.y + entity.sizeY - this.minY) / this.cellSize);
+            this.grid = Array(this.cGridWidth);
 
-        for (cX = cXEntityMin; cX <= cXEntityMax; cX++) {
-            if (!this.grid[cX]) {
-                this.grid[cX] = Array(this.cGridHeight);
-            }
+            for (var i = 0; i < this.entities.length; i++) {
+                entity = this.entities[i];
 
-            gridCol = this.grid[cX];
-
-            for (cY = cYEntityMin; cY < cYEntityMax; cY++) {
-                if (!gridCol[cY]) {
-                    gridCol[cY] = [];
+                //Check whether entity is outside the spatial grid
+                if (entity.x < this.minX || entity.x > this.maxX || entity.y < this.minY || entity.y > this.maxY) {
+                    continue;
                 }
 
-                gridCell = gridCol[cY];
+                cXEntityMin = Math.floor((entity.x - this.minX) / this.cellSize);
+                cXEntityMax = Math.floor((entity.x + entity.sizeX - this.minX) / this.cellSize);
+                cYEntityMin = Math.floor((entity.y - this.minY) / this.cellSize);
+                cYEntityMax = Math.floor((entity.y + entity.sizeY - this.minY) / this.cellSize);
 
-                gridCell.push(entity);
+                for (cX = cXEntityMin; cX <= cXEntityMax; cX++) {
+                    if (!this.grid[cX]) {
+                        this.grid[cX] = Array(this.cGridHeight);
+                    }
+
+                    gridCol = this.grid[cX];
+
+                    for (cY = cYEntityMin; cY < cYEntityMax; cY++) {
+                        if (!gridCol[cY]) {
+                            gridCol[cY] = [];
+                        }
+
+                        gridCell = gridCol[cY];
+
+                        gridCell.push(entity);
+                    }
+                }
             }
-        }
-    }
-};
+        };
 
-broadphase.spatialGrid.prototype.queryForCollisionPairs = function () {
-    var checked = {}, pairs = [], gridCol, gridCell, i, j, k, l, entityA, entityB, hashA, hashB;
+        SpatialGrid.prototype.queryForCollisionPairs = function () {
+            var checked = {}, pairs = [], gridCol, gridCell, i, j, k, l, entityA, entityB, hashA, hashB;
 
-    for (i = 0; i < this.grid.length; i++) {
-        gridCol = this.grid[i];
+            for (i = 0; i < this.grid.length; i++) {
+                gridCol = this.grid[i];
 
-        if (!gridCol) {
-            continue;
-        }
+                if (!gridCol) {
+                    continue;
+                }
 
-        for (j = 0; j < gridCol.length; j++) {
-            gridCell = gridCol[j];
+                for (j = 0; j < gridCol.length; j++) {
+                    gridCell = gridCol[j];
 
-            if (!gridCell) {
-                continue;
-            }
+                    if (!gridCell) {
+                        continue;
+                    }
 
-            for (k = 0; k < gridCell.length; k++) {
-                entityA = gridCell[k];
+                    for (k = 0; k < gridCell.length; k++) {
+                        entityA = gridCell[k];
 
-                for (l = k + 1; l < gridCell.length; l++) {
-                    entityB = gridCell[l];
+                        for (l = k + 1; l < gridCell.length; l++) {
+                            entityB = gridCell[l];
 
-                    hashA = entityA.id + ":" + entityB.id;
-                    hashB = entityB.id + ":" + entityA.id;
+                            hashA = entityA.id + ":" + entityB.id;
+                            hashB = entityB.id + ":" + entityA.id;
 
-                    if (!checked[hashA] && !checked[hashB]) {
-                        checked[hashA] = checked[hashB] = true;
+                            if (!checked[hashA] && !checked[hashB]) {
+                                checked[hashA] = checked[hashB] = true;
 
-                        if (this.aabb2DIntersection(entityA, entityB)) {
-                            pairs.push([entityA, entityB]);
+                                if (this.aabb2DIntersection(entityA, entityB)) {
+                                    pairs.push([entityA, entityB]);
+                                }
+                            }
                         }
                     }
                 }
             }
-        }
-    }
 
-    return pairs;
-};
+            return pairs;
+        };
 
-broadphase.spatialGrid.prototype.aabb2DIntersection = function (objA, objB) {
-    var a = objA.getAABB(), b = objB.getAABB();
+        SpatialGrid.prototype.aabb2DIntersection = function (objA, objB) {
+            var a = objA.getAABB(), b = objB.getAABB();
 
-    if (a.x > b.x + b.size || a.y > b.y + b.size || a.x + a.size < b.x || a.y + a.size < b.y) {
-        return false;
-    } else {
-        return true;
-    }
-};
+            if (a.x > b.x + b.size || a.y > b.y + b.size || a.x + a.size < b.x || a.y + a.size < b.y) {
+                return false;
+            } else {
+                return true;
+            }
+        };
+        return SpatialGrid;
+    })();
+    MainModule.SpatialGrid = SpatialGrid;
+})(MainModule || (MainModule = {}));
